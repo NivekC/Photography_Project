2be5fdd7df15dac4d49f7ad5a1cb8601d11118f1
NivekC <kevin.chemjor@strathmore.edu>
2019-04-24 13:50:34 +0300

    Photography

---
+++ html/login.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Cocoon -Portfolio">
+        <meta name="keywords" content="Cocoon , Portfolio">
+        <meta name="author" content="Pharaohlab">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Log In  </title>
+        <!-- ========== Favicon Ico ========== -->
+        <!--<link rel="icon" href="fav.ico">-->
+        <!-- ========== STYLESHEETS ========== -->
+        <!-- Bootstrap CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Fonts Icon CSS -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="assets/css/et-line.css" rel="stylesheet">
+        <link href="assets/css/ionicons.min.css" rel="stylesheet">
+        <!-- Carousel CSS -->
+        <link href="assets/css/slick.css" rel="stylesheet">
+        <!-- Magnific-popup -->
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <!-- Animate CSS -->
+        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <!-- Custom styles for this template -->
+        <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+  
+</html>
